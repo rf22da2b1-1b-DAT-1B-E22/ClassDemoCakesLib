44ab08630864b638a24f3b133e604bac34128b43
Peter Levinsky <pele@zealand.dk>
2023-09-26 13:56:33 +0200

    Add project files.

---
+++ ClassDemoCakesLib/collection/CakeRepositoryService.cs
@@ -0,0 +1,116 @@
+﻿using ClassDemoCakesLib.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoCakesLib.collection
+{
+    public class CakeRepositoryService
+    {
+        /*
+         * collection of cakes
+         */
+
+        private List<Cake> cakes;
+
+        /*
+         * Constructor
+         */
+
+        public CakeRepositoryService(bool MedTestData = false) { 
+            cakes = new List<Cake>();
+
+            if (MedTestData)
+            {
+                PopulateCakes();
+            }
+        }
+
+        
+
+
+
+        /*
+         * Methods
+         * 
+         * CRUD etc.
+         */
+
+        public Cake Add(Cake cake)
+        {
+            cakes.Add(cake);
+
+            return cake;
+        }
+
+        public Cake Delete(int id)
+        {
+            Cake cake = GetById(id); // can throw an exception
+
+            cakes.Remove(cake);
+            return cake;
+
+        }
+
+        public Cake Edit(int id, Cake cake)
+        {
+            Cake foundCake = GetById(id); // can throw an exception
+
+            cakes[cakes.IndexOf(foundCake)] = cake;
+            return cake;
+
+        }
+
+        public Cake GetById(int id)
+        {
+            Cake? cake = cakes.Find(c => c.Id == id);
+
+            if (cake is null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            return cake;
+        }
+
+
+        public IEnumerable<Cake> GetAll()
+        {
+            return new List<Cake>(cakes);
+        }
+
+
+
+
+
+
+
+
+        /*
+         * For test purpose - add testdata
+         */
+        public void PopulateCakes()
+        {
+            cakes.Clear();
+
+            cakes.Add(new Cake(1, "Snegl", "wienerbrød med glasur", 23));
+            cakes.Add(new Cake(2, "Kaj Kage", "tørkage grøn med kræm", 31));
+            cakes.Add(new Cake(1, "Brunsviger", "gærbund godt med sukkerglasur", 26));
+            cakes.Add(new Cake(1, "Croissant", "wienerbrød", 19));
+            cakes.Add(new Cake(1, "Jordbærkage", "flødeskumskage med jordbær", 35));
+
+        }
+
+
+
+        /*
+         * ToString
+         */
+        public override string ToString()
+        {
+            return $"{{String.join(', ', cakes)}}";
+        }
+    }
+}
---
+++ ClassDemoCakesLib/model/Cake.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoCakesLib.model
+{
+    public class Cake
+    {
+        /*
+         * instans fields
+         */
+        private int _id;
+        private string _name;
+        private string _description;
+        private double _price;
+
+        /*
+         * properties
+         */
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public double Price { get { return _price; } set { _price = value; } }
+
+        /*
+         * constructors
+         */
+        public Cake() { 
+            _id = 0;
+            _name = string.Empty;
+            _description = string.Empty;
+            _price = 0;
+        }
+
+        // public Cake(): this(0, string.Empty, string.Empty, 0) {}
+
+        public Cake(int id, string name, string description, double price)
+        {
+            _id = id;
+            _name = name;
+            _description = description;
+            _price = price;
+        }
+
+
+        /*
+         * ToString
+         */
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Name)}={Name}, {nameof(Description)}={Description}, {nameof(Price)}={Price.ToString()}}}";
+        }
+
+
+
+    }
+}
