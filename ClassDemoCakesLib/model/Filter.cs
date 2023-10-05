224aff275022f18db1612f6d955dad9d59428ad5
Peter Levinsky <pele@zealand.dk>
2023-10-05 13:00:06 +0200

    Filter klasse / Record indsat

---
+++ ClassDemoCakesLib/model/Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoCakesLib.model
+{
+    public class Filter2
+    {
+        public int? MaxPris { get; set; }
+
+        // constructor
+        // tostring
+
+
+    }
+
+    public record Filter (int? MaxPris);
+}
